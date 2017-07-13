f2a9dbd5c7c8e0de8671452735467fe31195a893
Thomas Schellenberg <thomas.schellenberg@nextcentury.com>
2017-07-13 18:09:02 -0400

    Changed server config so that multiple search endpoints can now be defined.

--- server/routes.js
+++ server/routes.js
@@ -39,14 +39,12 @@ module.exports = function(app) {
       configEndpoint: serverConfig.configEndpoint,
       configPassword: serverConfig.configPassword,
       configUsername: serverConfig.configUsername,
+      databaseType: serverConfig.databaseType,
       defaultProject: serverConfig.defaultProject,
       esHost: serverConfig.esHost,
-      esIndex: serverConfig.esIndex,
-      esType: serverConfig.esType,
-      esDataEndpoint: serverConfig.esDataEndpoint,
       imageServiceAuth: serverConfig.imageServiceAuth,
       imageServiceHost: serverConfig.imageServiceHost,
-      searchEndpoint: serverConfig.searchEndpoint
+      searchConfig: serverConfig.searchConfig
     });
   });
 
