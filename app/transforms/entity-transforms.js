57cc8308119289409bf781dcc11d92e4659553fa
ryan <ryan.o.n.scott@gmail.com>
2017-07-21 09:34:24 -0400

    Fixing lint error

--- app/transforms/entity-transforms.js
+++ app/transforms/entity-transforms.js
@@ -319,7 +319,7 @@ var entityTransforms = (function(_, commonTransforms, esConfig) {
 
   function createDateObjectFromBucket(dateBucket, config) {
     var newData = [];
-    if (dateBucket && dateBucket[config.entity.key] && dateBucket[config.entity.key].buckets) {
+    if(dateBucket && dateBucket[config.entity.key] && dateBucket[config.entity.key].buckets) {
       newData = dateBucket[config.entity.key].buckets.map(function(entityBucket, index) {
         return getExtraction(entityBucket, config.entity, index);
       }).filter(commonTransforms.getExtractionFilterFunction(config.entity.type));
