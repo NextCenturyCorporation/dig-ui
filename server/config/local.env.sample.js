c657d345f8cfbb2000ab14ea7a88323435cec902
Thomas Schellenberg <thomas.schellenberg@nextcentury.com>
2017-08-17 11:29:11 -0400

    Updated README and sample files and package version.

--- server/config/local.env.sample.js
+++ server/config/local.env.sample.js
@@ -4,5 +4,8 @@ module.exports = {
   CONFIG_ENDPOINT: 'http://localhost:8080/config',
   DATABASE_TYPE: 'full',
   DEFAULT_PROJECT: 'project',
-  ES_HOST: '{"host":"http://localhost:9200"}'
+  ES_HOST: '{"host":"http://localhost:9200"}',
+  TAGS_ENTITY_ENDPOINT:'http://localhost:8080/projects/PROJECT/tags/TAG/types/ENTITY_TYPE/entities/ENTITY_ID',
+  TAGS_EXTRACTION_ENDPOINT:'http://localhost:8080/projects/PROJECT/tags/TAG/types/ENTITY_TYPE/entities/ENTITY_ID/fields/EXTRACTION_FIELD/keys/EXTRACTION_KEY',
+  TAGS_LIST_ENDPOINT:'http://localhost:8080/projects/PROJECT/tags'
 };
